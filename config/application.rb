1223bf57316c1c1592420e124362b959376eb361
Georgi Kirichkov <kirichkov@users.noreply.github.com>
2025-03-19 14:58:05 +0200

    Load rails 7.1 defaults

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ Bundler.require(*Rails.groups(assets: %w(development test)))
 
 module ComfortableMexicanSofa
   class Application < Rails::Application
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     require_relative '../lib/comfortable_mexican_sofa'
 
